5e5ddd28362d932234d591fc080cb16f33cee823
GIT-YanMartins <yangabriel15@outlook.com>
2025-06-19 16:57:36 -0300

    V2.1.2- Foram corrigidas as potenciais inseguranças com o uso indevido do target= blank e foram dicionados rel=noopener noreferrer para que o usuário não tenha acesso à página anterior ao acessar uma nova

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import * as F from "./footer_style.js";
-import { FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
+import { FaLinkedin, FaInstagram, FaEnvelope, FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <F.Footer id="contato">
       <F.SocialLinks>
-        <a href="https://www.linkedin.com/in/0yanmartins/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/0yanmartins/" rel="noopener noreferrer">
           <FaLinkedin size={24} />
         </a>
-        <a href="https://www.instagram.com/yaaanxp/" target="_blank" rel="noopener noreferrer">
-          <FaInstagram size={24} />
+        <a href="https://github.com/GIT-YanMartins" rel="noopener noreferrer">
+          <FaGithub size={24} />
         </a>
         <a href="mailto:yanmartins-@outlook.com">
           <FaEnvelope size={24} />
